8d61b4bdf87f33e97d2b066fca7c03dfc4daa634
Paul Lapsley <lapsley.p@gmail.com>
2020-12-02 22:04:57 -0600

    Added test classes

---
+++ src/test/java/org/apache/commons/mail/EmailAttachmentTest.java
@@ -0,0 +1,52 @@
+package org.apache.commons.mail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailAttachmentTest extends TestCase{
+    EmailAttachment testEmailAttachment;
+
+    @Before
+    public void setUp() {
+        testEmailAttachment = new EmailAttachment();
+    }
+
+    @Test
+    public void testSetName() {
+        System.out.println("EmailAttachmentTest: This test attempts to set the attachment's name");
+        System.out.println("gdpXXX");
+        testEmailAttachment.setName("portable_document.pdf");
+        assertEquals("portable_document.pdf", testEmailAttachment.getName());
+    }
+
+    @Test
+    public void testSetURL() {
+        System.out.println("EmailAttachmentTest: This test attempts to set the attachment's URL");
+        System.out.println("gdpXXX");
+        testEmailAttachment.setURL(new URL("https://www.dotcom.com"));
+        assertEquals("https://www.dotcom.com", testEmailAttachment.getURL().toString());
+    }
+
+    @Test
+    public void testSetDescription() {
+        System.out.println("EmailAttachmentTest: This test attempts to set the attachment's description");   
+        System.out.println("gdpXXX");
+        testEmailAttachment.setDescription("The Portable Document");
+        assertEquals("The Portable Document", testEmailAttachment.getDescription());
+    }
+
+    @Test
+    public void testSetDisposition() {
+        System.out.println("EmailAttachmentTest: This test attempts to set the attachment's disposition");   
+        System.out.println("gdpXXX");
+        testEmailAttachment.setDisposition("Disposition");
+        assertEquals("Disposition", testEmailAttachment.getDisposition());
+    }
+    
+}
---
+++ src/test/java/org/apache/commons/mail/MailTest.java
@@ -0,0 +1,43 @@
+package org.apache.commons.mail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MailTest extends TestCase{
+    Email testEmail;
+
+    @Before
+    public void setUp() {
+        testEmail = new SimpleEmail();
+    }
+
+    @Test
+    public void testAddTo() throws EmailException {
+        System.out.println("MailTest.java: This test should succeed in adding a 'TO' recipient to the email.");
+        System.out.println("gdpXXX");
+        testEmail.addTo("aaa@b.com");
+        assertEquals("aaa@b.com", testEmail.getToAddresses().get(0).toString());
+    }
+
+    @Test
+    public void testSetSubject() {
+        System.out.printlin("MailTest.java: This test should set the subject.");
+        System.out.println("gdpXXX");
+        testEmail.setSubject("Hello");
+        assertEquals("Hello", testEmail.getSubject());
+    }
+
+    @Test
+    public void testSetSentDate() {
+        System.out.println("MailTest.java: This test should set the sent date.");
+        System.out.println("gdpXXX");
+        testEmail.setSentDate(new Date(100L));
+        assertEquals(new Date(100L), testEmail.getSentDate());
+    }
+}
